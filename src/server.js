7ebfdbec50ff6fdbb13cf6b69b465ffdb711b646
elsa.de.alfonso <elsa.de.alfonso@gmail.com>
2022-01-15 03:12:05 +0100

    les1 ok

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
-import config from './config'
 import cors from 'cors'
-import { connect } from './utils/db'
+
+import { connect } from './utils/db' // lesson 1
+import config from './config' // lesson 1
 
 export const app = express()
 
@@ -14,6 +15,26 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+// lesson 1
+// create a route that sends back some json
+app.get('/data', (req, res) => {
+  res.json({ message: 'hello' })
+})
+
+// lesson 1
+// create a route that accepts json and logs it
+app.post('/data', (req, res) => {
+  console.log(req.body)
+  res.status(200).end()
+})
+
+// lesson 1
+// start the server - type on shell
+//    brew services start mongodb-community@5.0
+//    npm run dev
+
+// lesson 1
+// empty start binding filled with this code to connect to db
 export const start = async () => {
   try {
     await connect()
