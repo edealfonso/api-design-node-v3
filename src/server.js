60b024db1ffa2aaec17f1990ee2f60efc25783d2
elsa.de.alfonso <elsa.de.alfonso@gmail.com>
2022-01-15 03:00:44 +0100

    lesson1 ok

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
 
+import { connect } from './utils/db' // lesson 1
+import config from './config' // lesson 1
+
 export const app = express()
 
 app.disable('x-powered-by')
@@ -12,4 +15,33 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+// lesson 1
+// create a route that sends back some json
+app.get('/data', (req, res) => {
+  res.json({ message: 'hello' })
+})
+
+// lesson 1
+// create a route that accepts json and logs it
+app.post('/data', (req, res) => {
+  console.log(req.body)
+  res.status(200).end()
+})
+
+// lesson 1
+// start the server - type on shell: 
+//    brew services start mongodb-community@5.0
+//    npm run dev
+
+// lesson 1
+// empty start binding filled with this code to connect to db
+export const start = async () => {
+  try {
+    await connect()
+    app.listen(config.port, () => {
+      console.log(`REST API on http://localhost:${config.port}/api`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
